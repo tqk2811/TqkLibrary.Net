d3ff1f148e9569e04c1ac6f0d9bf6173aad83567
tqk2811 <tqk2811@gmail.com>
2021-06-30 15:05:52 +0700

    FptAiTest TextToSpeechTest

--- TqkLibrary.Net.Test/FptAiTest.cs
+++ TqkLibrary.Net.Test/FptAiTest.cs
@@ -10,8 +10,21 @@ namespace TqkLibrary.Net.Test
     [TestMethod]
     public void TextToSpeechTest()
     {
+      string text = @"Áo sơ mi tay dài nữ - Lụa basic - Bạc grey
+- Chất liệu : Lụa cao cấp
+- Màu sắc : Trắng - Hồng - Bạc ( Silver Grey - Silver Gold)
+- Kiểu dáng : Áo somi tay dài basic
+- Loại hàng : Áo somi tay dài basic đi làm, đi chơi
+- Kích thước : Size S/M
+Tay : 55
+Vai : 42
+Ngực : 86
+Dài : 45
+Eo : 84
+-----------------
+Mọi người có thể FOLLOW mình tại đây nhé!!!";
       TextToSpeech textToSpeech = new TextToSpeech("IYvSNfjOQPSVKg7KZsBb2oFXTI0yDiD8");
-      TTSResponse response = textToSpeech.TTS("Nguyễn Hải Long").Result;
+      TTSResponse response = textToSpeech.TTS(text).Result;
     }
   }
 }
