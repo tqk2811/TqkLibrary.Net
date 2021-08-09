cd9301a39a34cc8d5acdfab7e31656e067e736f2
tqk2811 <tqk2811@gmail.com>
2021-08-09 18:47:18 +0700

    update chothuesimcode, add smmfansfaster

--- TqkLibrary.Net/PhoneNumberApi/ChoThueSimCodeApi.cs
+++ TqkLibrary.Net/PhoneNumberApi/ChoThueSimCodeApi.cs
@@ -51,8 +51,8 @@ namespace TqkLibrary.Net.PhoneNumberApi
   {
     public string SMS { get; set; }
     public string Code { get; set; }
-    public double Cost { get; set; }
-    public bool IsCall { get; set; }
+    public double? Cost { get; set; }
+    public bool? IsCall { get; set; }
     public string CallFile { get; set; }
     public string CallText { get; set; }
   }
