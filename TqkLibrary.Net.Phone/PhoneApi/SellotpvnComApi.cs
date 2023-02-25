f3c8f92a03f2091b8c1c5d634acb6f8169db5e2f
tqk2811 <tqk2811@gmail.com>
2023-02-25 19:41:17 +0700

    add SellotpvnComApi

---
+++ TqkLibrary.Net.Phone/PhoneApi/SellotpvnComApi.cs
@@ -0,0 +1,131 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Policy;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace TqkLibrary.Net.Phone.PhoneApi
+{
+    public class SellotpvnComApi : BaseApi
+    {
+        public enum NetworkProvider
+        {
+            Viettel,
+            Mobifone,
+            Vinaphone,
+            Vietnamobile,
+            Itel
+        }
+        public class Serivce
+        {
+            public Serivce(string serviceName)
+            {
+                if (string.IsNullOrWhiteSpace(serviceName)) throw new ArgumentNullException(nameof(serviceName));
+                this.ServiceName = serviceName;
+            }
+            public string ServiceName { get; }
+
+            public static readonly Serivce DOSI = new("dosi");
+            public static readonly Serivce Facebook = new("facebook");
+            public static readonly Serivce Telegram = new("telegram");
+            public static readonly Serivce Shopee = new("shopee");
+            public static readonly Serivce Hotmail = new("hotmail");
+            public static readonly Serivce Gmail = new("gmail");
+            public static readonly Serivce Zalo = new("zalo");
+            public static readonly Serivce KokPlay = new("kokplay");
+            public static readonly Serivce Wechat = new("wechat");
+            public static readonly Serivce Onjoyride = new("joyride");
+            public static readonly Serivce Garena = new("garena");
+            public static readonly Serivce Lazada = new("lazada");
+            public static readonly Serivce SenDo = new("sendo");
+            public static readonly Serivce BBMeta = new("bbmeta");
+            public static readonly Serivce Instagram = new("insg");
+            public static readonly Serivce Discord = new("discord");
+            public static readonly Serivce TikTok = new("tiktok");
+            public static readonly Serivce Shbet = new("shbet");
+            public static readonly Serivce HappyPig = new("happy");
+        }
+
+        public class Response
+        {
+            [JsonProperty("error")]
+            public bool? Error { get; set; }
+            [JsonProperty("id")]
+            public int Id { get; set; }
+
+            [JsonProperty("phoneNumber")]
+            public string PhoneNumber { get; set; }
+
+            [JsonProperty("serviceName")]
+            public string ServiceName { get; set; }
+
+            [JsonProperty("content")]
+            public string Content { get; set; }
+
+            [JsonProperty("status")]
+            public Status Status { get; set; }
+
+            [JsonProperty("createdAt")]
+            public string CreatedAt { get; set; }
+        }
+        public enum Status
+        {
+            Failed,
+            Successed,
+            Pending
+        }
+
+
+        public class ApiEndPoint
+        {
+            public ApiEndPoint(Uri uri)
+            {
+                this.Uri = uri ?? throw new ArgumentNullException(nameof(uri));
+            }
+            public Uri Uri { get; }
+
+            public static readonly ApiEndPoint SellotpVn = new ApiEndPoint(new Uri("https://sellotpvn.com/api/gsm"));
+            public static readonly ApiEndPoint NumberotpCo = new ApiEndPoint(new Uri("https://numberotp.co/api/gsm"));
+        }
+
+
+        readonly ApiEndPoint apiEndPoint;
+        public SellotpvnComApi(ApiEndPoint apiEndPoint, string token) : base(token)
+        {
+            this.apiEndPoint = apiEndPoint ?? throw new ArgumentNullException(nameof(apiEndPoint));
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="serivce"></param>
+        /// <param name="networkProvider">Null for random</param>
+        /// <param name="cancellationToken"></param>
+        /// <returns></returns>
+        public virtual Task<Response> Order(
+            Serivce serivce,
+            NetworkProvider? networkProvider = null,
+            CancellationToken cancellationToken = default)
+        {
+            if (serivce is null) throw new ArgumentNullException(nameof(serivce));
+
+            return Build()
+                .WithUrlGet(new UriBuilder(apiEndPoint.Uri, "order", serivce.ServiceName, ApiKey)
+                                .WithParamIfNotNull("provider", networkProvider))
+                .ExecuteAsync<Response>(cancellationToken);
+        }
+
+        public virtual Task<Response> GetOrder(
+            Response response,
+            CancellationToken cancellationToken = default)
+        {
+            if (response is null) throw new ArgumentNullException(nameof(response));
+            return Build()
+                .WithUrlGet(new UriBuilder(apiEndPoint.Uri, "get-order", response.Id, ApiKey))
+                .ExecuteAsync<Response>(cancellationToken);
+        }
+    }
+}
---
+++ TqkLibrary.Net.Phone/PhoneApi/Wrapper/Implements/SellotpvnComApiWrapper.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace TqkLibrary.Net.Phone.PhoneApi.Wrapper.Implements
+{
+    public class SellotpvnComApiWrapper : IPhoneWrapper
+    {
+        readonly SellotpvnComApi sellotpvnComApi;
+        public SellotpvnComApiWrapper(SellotpvnComApi.ApiEndPoint apiEndPoint, string token) : this(new SellotpvnComApi(apiEndPoint, token))
+        {
+
+        }
+        public SellotpvnComApiWrapper(SellotpvnComApi sellotpvnComApi)
+        {
+            this.sellotpvnComApi = sellotpvnComApi ?? throw new ArgumentNullException(nameof(sellotpvnComApi));
+        }
+
+        public SellotpvnComApi.Serivce Service { get; set; }
+        public SellotpvnComApi.NetworkProvider? NetworkProvider { get; set; }
+
+        public void Dispose()
+        {
+            sellotpvnComApi.Dispose();
+        }
+
+        public async Task<IPhoneWrapperSession> RentPhone(CancellationToken cancellationToken = default)
+        {
+            if (Service is null) throw new InvalidOperationException($"{nameof(SellotpvnComApiWrapper)}.{nameof(Service)} musbe set");
+            var response = await sellotpvnComApi.Order(Service, NetworkProvider, cancellationToken).ConfigureAwait(false);
+            return new PhoneWrapperSession(sellotpvnComApi, response);
+        }
+
+        class PhoneWrapperSession : IPhoneWrapperSession
+        {
+            readonly SellotpvnComApi sellotpvnComApi;
+            readonly SellotpvnComApi.Response response;
+            public PhoneWrapperSession(SellotpvnComApi sellotpvnComApi, SellotpvnComApi.Response response)
+            {
+                this.sellotpvnComApi = sellotpvnComApi ?? throw new ArgumentNullException(nameof(sellotpvnComApi));
+                this.response = response ?? throw new ArgumentNullException(nameof(response));
+            }
+            public bool IsSuccess => response.Status == SellotpvnComApi.Status.Pending;
+
+            public string PhoneNumber => response.PhoneNumber;
+
+            public string Message => response.otp;
+
+            public Task CancelWaitSms(CancellationToken cancellationToken = default)
+            {
+                throw new NotImplementedException();
+            }
+
+            public Task<IPhoneWrapperSmsResult<IPhoneWrapperSms>> GetSms(CancellationToken cancellationToken = default)
+            {
+                throw new NotImplementedException();
+            }
+        }
+    }
+}
