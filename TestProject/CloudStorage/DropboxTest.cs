c9330c11ff38929a4f31cd6d7f84ddfa3c56431d
tqk2811 <tqk2811@gmail.com>
2023-10-14 10:35:53 +0700

    update dropbox test

--- TestProject/CloudStorage/DropboxTest.cs
+++ TestProject/CloudStorage/DropboxTest.cs
@@ -25,6 +25,9 @@ namespace TestProject.CloudStorage
             get
             {
                 yield return new object[] { "https://www.dropbox.com/scl/fi/0sskovcwdudgl97x2fuz1/Intro.mp4?rlkey=qxp0gw0wxzbs9bfedskderdwx&dl=0" };
+                yield return new object[] { "https://www.dropbox.com/scl/fi/8bolsdza80qsahhr083m3/Loop.mp4?rlkey=rzh16dzz3v9ng4hecftsouy8y&dl=0" };
+                yield return new object[] { "https://www.dropbox.com/scl/fi/u6rm8g4yjohpqwjjiywsq/loop.mp3?rlkey=n7ovrti3rbvnld0khdilz5n54&dl=0" };
+                yield return new object[] { "https://www.dropbox.com/scl/fi/miggmk5geszp1cyfa1ech/outro.mp3?rlkey=nal532629t29y24tj9fhvqf9h&dl=0" };
             }
         }
 
