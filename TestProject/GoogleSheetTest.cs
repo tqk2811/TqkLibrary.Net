ee7f0f46a3201ffa05228f402e0c3544c747a7e4
tqk2811 <tqk2811@gmail.com>
2023-11-04 18:06:12 +0700

    fix GoogleSheet

---
+++ TestProject/GoogleSheetTest.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TqkLibrary.Net.Other.Others;
+namespace TestProject
+{
+    [TestClass]
+    public class GoogleSheetTest
+    {
+        readonly GoogleSheet _googleSheet = new GoogleSheet();
+        public static IEnumerable<object[]> Ids
+        {
+            get
+            {
+                yield return new object[] { "1olq8d_cPt6BlLSnql3zSVmNsMa8uJCD-9qpRzpSHR6U" };
+            }
+        }
+
+
+        [TestMethod(), DynamicData(nameof(Ids))]
+        public async Task ExportTest(string id)
+        {
+            using var stream = await _googleSheet.ExportAsync(id);
+            using MemoryStream memoryStream = new MemoryStream();
+            await stream.CopyToAsync(memoryStream);
+        }
+    }
+}
--- TqkLibrary.Net.Other/Others/GoogleSheet.cs
+++ TqkLibrary.Net.Other/Others/GoogleSheet.cs
@@ -2,9 +2,11 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
+using TqkLibrary.Net.HttpClientHandles;
 
 namespace TqkLibrary.Net.Other.Others
 {
@@ -16,9 +18,43 @@ namespace TqkLibrary.Net.Other.Others
         /// <summary>
         /// 
         /// </summary>
-        public GoogleSheet()
+        public GoogleSheet() : this(new CookieHandler(new HttpClientHandler()
+        {
+            AllowAutoRedirect = false,
+        }))
         {
             this.httpClient.DefaultRequestHeaders.Referrer = new Uri("https://docs.google.com");
+            //this.httpClient.DefaultRequestHeaders.Add("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7");
+            //this.httpClient.DefaultRequestHeaders.Add("Accept-Language", "en");
+            //this.httpClient.DefaultRequestHeaders.Add("Accept-Encoding", "gzip, deflate, br");
+            //this.httpClient.DefaultRequestHeaders.Add("Dnt", "1");
+
+            //this.httpClient.DefaultRequestHeaders.Add("Sec-Ch-Ua", "\"Chromium\";v=\"118\", \"Google Chrome\";v=\"118\", \"Not=A?Brand\";v=\"99\"");
+            //this.httpClient.DefaultRequestHeaders.Add("Sec-Ch-Ua-Arch", "\"x86\"");
+            //this.httpClient.DefaultRequestHeaders.Add("Sec-Ch-Ua-Bitness", "\"64\"");
+            //this.httpClient.DefaultRequestHeaders.Add("Sec-Ch-Ua-Full-Version-List", "\"Chromium\";v=\"118.0.5993.120\", \"Google Chrome\";v=\"118.0.5993.120\", \"Not=A?Brand\";v=\"99.0.0.0\"");
+            //this.httpClient.DefaultRequestHeaders.Add("", "");
+            //this.httpClient.DefaultRequestHeaders.Add("", "");
+            //this.httpClient.DefaultRequestHeaders.Add("", "");
+            //this.httpClient.DefaultRequestHeaders.Add("", "");
+            //this.httpClient.DefaultRequestHeaders.Add("", "");
+            //this.httpClient.DefaultRequestHeaders.Add("", "");
+            //this.httpClient.DefaultRequestHeaders.Add("Sec-Ch-Ua-Platform-Version", "\"10.0.0\"");
+            //this.httpClient.DefaultRequestHeaders.Add("Sec-Ch-Ua-Mobile", "?0");
+            //this.httpClient.DefaultRequestHeaders.Add("Sec-Ch-Ua-Model", "\"\"");
+            //this.httpClient.DefaultRequestHeaders.Add("Sec-Ch-Ua-Platform", "\"Windows\"");
+            //this.httpClient.DefaultRequestHeaders.Add("Sec-Fetch-Dest", "iframe");
+            //this.httpClient.DefaultRequestHeaders.Add("Sec-Fetch-Mode", "navigate");
+            //this.httpClient.DefaultRequestHeaders.Add("Sec-Fetch-Site", "same-origin");
+            //this.httpClient.DefaultRequestHeaders.Add("Sec-Fetch-User", "?1");
+
+            //this.httpClient.DefaultRequestHeaders.Add("Upgrade-Insecure-Requests", "1");
+            //this.httpClient.DefaultRequestHeaders.Add("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/118.0.0.0 Safari/537.36");
+        }
+        readonly CookieHandler cookieHandler;
+        private GoogleSheet(CookieHandler cookieHandler) : base(".", cookieHandler)
+        {
+            this.cookieHandler = cookieHandler;
         }
         /// <summary>
         /// 
@@ -29,15 +65,26 @@ namespace TqkLibrary.Net.Other.Others
         /// <returns></returns>
         public async Task<Stream> ExportAsync(string fileId, ExportExcelType format = ExportExcelType.xlsx, CancellationToken cancellationToken = default)
         {
+            //var res_html = await Build()
+            //    .WithUrlGet(new UrlBuilder("https://docs.google.com/spreadsheets/d", fileId, "edit"))
+            //    .ExecuteAsync(cancellationToken);
+
             var res = await Build()
                 .WithUrlGet(new UrlBuilder("https://docs.google.com/spreadsheets/d", fileId, "export")
                     .WithParam("format", format)
                     .WithParam("id", fileId))
                 .ExecuteAsync(cancellationToken);
+            if (res.Headers.Location is null)
+            {
+                res.EnsureSuccessStatusCode();
+            }
+            var res2 = await Build()
+                .WithUrlGet(res.Headers.Location)
+                .ExecuteAsync(cancellationToken);
             try
             {
-                var stream = await res.EnsureSuccessStatusCode().Content.ReadAsStreamAsync();
-                return new HttpResponseStreamWrapper(res, stream);
+                var stream = await res2.EnsureSuccessStatusCode().Content.ReadAsStreamAsync();
+                return new HttpResponseStreamWrapper(res2, stream);
             }
             catch
             {
