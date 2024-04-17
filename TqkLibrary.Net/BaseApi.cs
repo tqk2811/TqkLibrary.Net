f94aefd25d4160956246a1aa5efa8c67ec62f02a
tqk2811 <tqk2811@gmail.com>
2024-04-17 10:24:47 +0700

    move ApiException

--- TqkLibrary.Net/BaseApi.cs
+++ TqkLibrary.Net/BaseApi.cs
@@ -3,45 +3,6 @@ using System.Net;
 using System.Net.Http;
 namespace TqkLibrary.Net
 {
-    /// <summary>
-    /// 
-    /// </summary>
-    public class ApiException : Exception
-    {
-        /// <summary>
-        /// 
-        /// </summary>
-        public HttpStatusCode StatusCode { get; set; }
-
-        /// <summary>
-        /// 
-        /// </summary>
-        /// <returns></returns>
-        public override string ToString()
-        {
-            return $"{nameof(ApiException)}: {StatusCode}";
-        }
-    }
-
-    /// <summary>
-    /// 
-    /// </summary>
-    /// <typeparam name="T"></typeparam>
-    public class ApiException<T> : ApiException
-    {
-        /// <summary>
-        /// 
-        /// </summary>
-        public ApiException()
-        {
-
-        }
-
-        /// <summary>
-        /// 
-        /// </summary>
-        public T Body { get; set; }
-    }
 
     /// <summary>
     /// 
