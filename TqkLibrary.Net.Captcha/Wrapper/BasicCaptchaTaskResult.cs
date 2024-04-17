4fa2e53d766c190804accb4ee984426a07beb4c6
tqk2811 <tqk2811@gmail.com>
2024-04-17 19:30:20 +0700

    BasicCaptchaTaskResult nullable

--- TqkLibrary.Net.Captcha/Wrapper/BasicCaptchaTaskResult.cs
+++ TqkLibrary.Net.Captcha/Wrapper/BasicCaptchaTaskResult.cs
@@ -3,7 +3,7 @@
     public class BasicCaptchaTaskResult
     {
         public bool IsSuccess { get; set; }
-        public string Value { get; set; }
-        public string ErrorMessage { get; set; }
+        public string? Value { get; set; }
+        public string? ErrorMessage { get; set; }
     }
 }
