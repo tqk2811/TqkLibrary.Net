5f3128b75e119aced5e577c762610ad3aacb99ca
tqk2811 <tqk2811@gmail.com>
2024-10-18 18:00:28 +0700

    add SlnExtensions

---
+++ SlnExtensions.cs
@@ -0,0 +1,13 @@
+﻿#if !NET5_0_OR_GREATER
+using System;
+namespace TqkLibrary.Net
+{
+    internal static class SlnExtensions
+    {
+        public static bool Contains(this string text, string value, StringComparison stringComparison)
+        {
+            return text.IndexOf(value, stringComparison) >= 0;
+        }
+    }
+}
+#endif
