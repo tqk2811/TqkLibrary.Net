ff07bb6544e5554cfd2d27e6b56158c133ca6a16
tqk2811 <tqk2811@gmail.com>
2025-05-24 16:22:32 +0700

    update test

--- ConsoleTest/Mail/TempMailOrgTest.cs
+++ ConsoleTest/Mail/TempMailOrgTest.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using TqkLibrary.Net.Mail.TempMails;
+using TqkLibrary.Net.Mail.Services.TempMails;
 
 namespace ConsoleTest.Mail
 {
