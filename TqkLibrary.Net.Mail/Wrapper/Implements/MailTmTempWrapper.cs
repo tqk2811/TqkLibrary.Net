50ed2a2953fe5202af3098e8193fa0dbc4854292
tqk2811 <tqk2811@gmail.com>
2025-05-24 16:30:55 +0700

    fix build

--- TqkLibrary.Net.Mail/Wrapper/Implements/MailTmTempWrapper.cs
+++ TqkLibrary.Net.Mail/Wrapper/Implements/MailTmTempWrapper.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
+using TqkLibrary.Net.Mail.Services.TempMails;
 
 namespace TqkLibrary.Net.Mail.Wrapper.Implements
 {
