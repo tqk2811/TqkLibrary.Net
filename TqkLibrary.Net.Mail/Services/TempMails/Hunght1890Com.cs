c63624712a1a4142a5d9d3226f1626448258c516
tqk2811 <tqk2811@gmail.com>
2025-06-02 11:21:46 +0700

    add Hunght1890Com

---
+++ TqkLibrary.Net.Mail/Services/TempMails/Hunght1890Com.cs
@@ -0,0 +1,63 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace TqkLibrary.Net.Mail.Services.TempMails
+{
+    public class Hunght1890Com : BaseApi
+    {
+        const string _Endpoint = "https://hunght1890.com";
+        public Hunght1890Com()
+        {
+
+        }
+
+        public Task<ConfigureData> GetConfigureAsync(CancellationToken cancellationToken = default)
+            => Build()
+            .WithUrlGet(new UrlBuilder(_Endpoint, "static/config.json"))
+            .ExecuteAsync<ConfigureData>(cancellationToken);
+
+
+
+
+        public Task<List<MailData>> GetMailsAsync(string address, CancellationToken cancellationToken = default)
+            => Build()
+            .WithUrlGet(new UrlBuilder(_Endpoint, address))
+            .WithCheckStatusCode(false)
+            .ExecuteAsync<List<MailData>>();
+
+
+
+
+
+
+        public class ConfigureData
+        {
+            [JsonProperty("domains")]
+            public List<string>? Domains { get; set; }
+        }
+        public class MailData
+        {
+            [JsonProperty("body")]
+            public string? Body { get; set; }
+
+            [JsonProperty("from")]
+            public string? From { get; set; }
+
+            [JsonProperty("subject")]
+            public string? Subject { get; set; }
+
+            /// <summary>
+            /// YYYY-MM-dd HH:mm:ss (utc)
+            /// </summary>
+            [JsonProperty("timestamp")]
+            public string? Timestamp { get; set; }
+
+            [JsonProperty("to")]
+            public string? To { get; set; }
+        }
+    }
+}
