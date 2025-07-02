6afd71cd0530e51d89b1aa34f0faea2949521cde
tqk2811 <tqk2811@gmail.com>
2025-07-02 17:41:45 +0700

    add DisableFindIpV6

--- TqkLibrary.Net/NetExtensions.cs
+++ TqkLibrary.Net/NetExtensions.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http;
+using System.Net.Sockets;
 using System.Threading.Tasks;
 
 namespace TqkLibrary.Net
@@ -18,5 +19,18 @@ namespace TqkLibrary.Net
         /// <param name="uri"></param>
         /// <returns></returns>
         public static Uri GetDomain(this Uri uri) => new Uri($"{uri.Scheme}://{uri.Authority}");
+
+#if NET5_0_OR_GREATER
+        public static SocketsHttpHandler DisableFindIpV6(this SocketsHttpHandler handler)
+        {
+            handler.ConnectCallback = async (context, cancellationToken) =>
+            {
+                Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+                await socket.ConnectAsync(context.DnsEndPoint.Host, context.DnsEndPoint.Port);
+                return new NetworkStream(socket, true);
+            };
+            return handler;
+        }
+#endif
     }
 }
