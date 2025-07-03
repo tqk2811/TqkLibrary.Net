b3b670881dc0d0f03850a4cfab778d7653392c6c
tqk2811 <tqk2811@gmail.com>
2025-07-03 15:24:29 +0700

    add ZingproxyComApi

---
+++ TqkLibrary.Net.Proxy/Services/ZingproxyComApi.cs
@@ -0,0 +1,85 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace TqkLibrary.Net.Proxy.Services
+{
+    public class ZingproxyComApi : BaseApi
+    {
+        const string EndPoint = "https://api.zingproxy.com";
+        public ZingproxyComApi(string apiKey) : base(apiKey)
+        {
+        }
+
+
+
+        public Task<ProxyResponse> GetProxy(CancellationToken cancellationToken = default) 
+            => Build()
+                .WithUrlGet(new UrlBuilder(EndPoint, "get-proxy").WithParam("key", ApiKey!))
+                .ExecuteAsync<ProxyResponse>(cancellationToken);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        public class ProxyData
+        {
+            [JsonProperty("uId")]
+            public string? UId { get; set; }
+
+            [JsonProperty("resourceId")]
+            public string? ResourceId { get; set; }
+
+            [JsonProperty("httpProxy")]
+            public string? HttpProxy { get; set; }
+
+            [JsonProperty("socks5Proxy")]
+            public string? Socks5Proxy { get; set; }
+
+            [JsonProperty("createdAt")]
+            public DateTime? CreatedAt { get; set; }
+
+            [JsonProperty("dateEnd")]
+            public DateTime? DateEnd { get; set; }
+
+            [JsonProperty("autoRenew")]
+            public bool? AutoRenew { get; set; }
+        }
+
+        public class BaseResponse
+        {
+            [JsonProperty("status")]
+            public ResponseStatus Status { get; set; }
+
+            [JsonProperty("error")]
+            public string? Error { get; set; }
+        }
+        public class ProxyResponse : BaseResponse
+        {
+            [JsonProperty("proxy")]
+            public ProxyData? Proxy { get; set; }
+        }
+
+        public enum ResponseStatus
+        {
+            Success,
+            Error,
+        }
+    }
+}
---
+++ TqkLibrary.Net.Proxy/Wrapper/Implements/ZingproxyComApiWrapper.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using TqkLibrary.Net.Proxy.Services;
+using TqkLibrary.Net.Proxy.Wrapper.Enums;
+using TqkLibrary.Net.Proxy.Wrapper.Interfaces;
+
+namespace TqkLibrary.Net.Proxy.Wrapper.Implements
+{
+    public class ZingproxyComApiWrapper : IProxyApiWrapper
+    {
+        /// <summary>
+        /// socks5 or http proxy
+        /// </summary>
+        public ProxyType ProxyType { get; set; } = ProxyType.Http;
+        public bool IsAllowGetNewOnUsing => false;
+
+
+        readonly ZingproxyComApi _zingproxyComApi;
+        public ZingproxyComApiWrapper(ZingproxyComApi zingproxyComApi)
+        {
+            this._zingproxyComApi = zingproxyComApi;
+        }
+        public ZingproxyComApiWrapper(string apiKey) : this(new ZingproxyComApi(apiKey))
+        {
+
+        }
+
+
+        public virtual async Task<IProxyApiResponseWrapper?> GetNewProxyAsync(CancellationToken cancellationToken)
+        {
+            var res = await _zingproxyComApi.GetProxy(cancellationToken);
+
+            ProxyApiResponseWrapper result = new ProxyApiResponseWrapper()
+            {
+                IsSuccess = res.Status == ZingproxyComApi.ResponseStatus.Success,
+                Message = res.Error
+            };
+            if (result.IsSuccess)
+            {
+                result.NextTime = DateTime.Now;
+                result.ExpiredTime = res.Proxy?.DateEnd;
+                if (ProxyType.HasFlag(ProxyType.Http) && !string.IsNullOrWhiteSpace(res.Proxy?.HttpProxy))
+                {
+                    result.Proxy = ProxyInfo.ParseHttpProxy(res.Proxy!.HttpProxy);
+                }
+                else if (ProxyType.HasFlag(ProxyType.Socks5) && !string.IsNullOrWhiteSpace(res.Proxy?.Socks5Proxy))
+                {
+                    result.Proxy = ProxyInfo.ParseHttpProxy(res.Proxy!.Socks5Proxy);
+                    if (result.Proxy is not null)
+                    {
+                        result.Proxy.ProxyType = ProxyType.Socks5;
+                    }
+                }
+                else if (!string.IsNullOrWhiteSpace(res.Proxy?.HttpProxy))
+                {
+                    result.Proxy = ProxyInfo.ParseHttpProxy(res.Proxy!.HttpProxy);
+                }
+                else if (!string.IsNullOrWhiteSpace(res.Proxy?.Socks5Proxy))
+                {
+                    result.Proxy = ProxyInfo.ParseHttpProxy(res.Proxy!.Socks5Proxy);
+                    if (result.Proxy is not null)
+                    {
+                        result.Proxy.ProxyType = ProxyType.Socks5;
+                    }
+                }
+
+                result.IsSuccess = result.Proxy is not null;
+            }
+            return result;
+        }
+    }
+}
