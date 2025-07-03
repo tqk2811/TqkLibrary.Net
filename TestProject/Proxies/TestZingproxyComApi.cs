5421dc6c7176923b3931a2ebac99293609b1c241
tqk2811 <tqk2811@gmail.com>
2025-07-03 20:10:11 +0700

    test TestZingproxyComApi

---
+++ TestProject/Proxies/TestZingproxyComApi.cs
@@ -0,0 +1,31 @@
+﻿
+using TqkLibrary.Net;
+using TqkLibrary.Net.Proxy.Services;
+
+namespace TestProject.Proxies
+{
+    [TestClass]
+    public class TestZingproxyComApi
+    {
+        public static IEnumerable<object[]> Datas
+        {
+            get
+            {
+                yield return new object[]
+                {
+                    "eyJhbGciOiJIU",
+                };
+            }
+        }
+        [TestMethod(), DynamicData(nameof(Datas))]
+        public async Task Test(string accessToken)
+        {
+            using SocketsHttpHandler socketsHttpHandler = new SocketsHttpHandler();
+            socketsHttpHandler.DisableFindIpV6();
+            using ZingproxyComApi zingproxyComApi = new(accessToken, socketsHttpHandler, false);
+            var list = await zingproxyComApi.DanCuVietNam.ListAsync(ZingproxyComApi.RunningType.Running);
+            var changed = await zingproxyComApi.DanCuVietNam.GetIpAsync(list.Proxies!.First());
+            var proxy = await zingproxyComApi.GetProxy(list.Proxies!.First()!);
+        }
+    }
+}
