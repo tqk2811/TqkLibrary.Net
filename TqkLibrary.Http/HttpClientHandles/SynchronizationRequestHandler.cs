1d1de24a739c71d95f090c9d977b608f5f79bb9a
tqk2811 <tqk2811@gmail.com>
2026-03-30 16:35:26 +0700

    add SynchronizationRequestHandler

---
+++ TqkLibrary.Http/HttpClientHandles/SynchronizationRequestHandler.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace TqkLibrary.Http.HttpClientHandles
+{
+    public class SynchronizationRequestHandler : DelegatingHandler
+    {
+        readonly SemaphoreSlim _semaphore = new SemaphoreSlim(1, 1);
+        public SynchronizationRequestHandler(HttpMessageHandler innerHandler) : base(innerHandler ?? throw new ArgumentNullException(nameof(innerHandler)))
+        {
+
+        }
+        protected override async Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken = default)
+        {
+            await _semaphore.WaitAsync(cancellationToken).ConfigureAwait(false);
+            try
+            {
+                return await base.SendAsync(request, cancellationToken).ConfigureAwait(false);
+            }
+            finally
+            {
+                _semaphore.Release();
+            }
+        }
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                _semaphore.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
